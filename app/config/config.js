7c6dfd44b424562f9f8b2ed56f6c0a6fb33aa682
Sebastian Vicario <svicario@cys.com.ar>
2019-09-26 20:06:03 -0300

    change client_id

--- app/config/config.js
+++ app/config/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   google: {
-      webClientId: '942857236809-1mbatv1f1t1eanl1jqrl6qjdjb0lu174.apps.googleusercontent.com',
+      webClientId: '942857236809-2qjq91t6661aqo83kfraeffcdb10dg42.apps.googleusercontent.com',
       //la de abajo era la nueva q armo seba
       // webClientId: '942857236809-2qjq91t6661aqo83kfraeffcdb10dg42.apps.googleusercontent.com',
       offlineAccess: false,
